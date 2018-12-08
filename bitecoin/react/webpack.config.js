083597bbb7d280470d0bb181ee998094f5fdc650
ning-cai <ning.cai@gmx.de>
2018-12-08 23:22:32 +0100

    Feature: create react package in project

---
+++ bitecoin/react/webpack.config.js
@@ -0,0 +1,44 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/App.js",
+  devtool: "sourcemaps",
+  cache: true,
+  mode: "development",
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "babel-loader"
+          },
+          {
+            loader: "react-svg-loader",
+            options: {
+              jsx: true // true outputs JSX tags
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
